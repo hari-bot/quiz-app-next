2d499ab6a79884445664ba21d7e086b74b9745fa
hariharan <hariharan@mojowebtech.com>
2025-01-19 18:45:50 +0530

    fixed empty interfaces error in input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
